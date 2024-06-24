a6a186a4b089cb4e418e2ef753b9d9f77e797207
dimaqqw <dimaaqqw@mail.ru>
2024-06-24 13:56:08 +0300

    add sort, filter by tags, pagination and etc...

--- controllers/meetupController.js
+++ controllers/meetupController.js
@@ -1,8 +1,61 @@
 const { Meetup } = require('../models')
+const { Op } = require('sequelize')
 const { meetupSchema } = require('../validators/meetupValidator')
 
 exports.getAllMeetups = async (req, res) => {
-  // ... (фильтрация, сортировка, пагинация)
+  try {
+    const {
+      search,
+      tags,
+      sortByName,
+      sortByDate,
+      page = 1,
+      size = 5,
+    } = req.query
+
+    // Фильтрация
+    const filter = {}
+    if (search) {
+      filter.title = { [Op.iLike]: `%${search}%` }
+    }
+    if (tags) {
+      filter.tags = { [Op.contains]: tags.split(',') }
+    }
+
+    // Сортировка
+    const order = []
+    if (sortByName) {
+      order.push(['title', sortByName])
+    }
+    if (sortByDate) {
+      order.push(['date', sortByDate])
+    }
+
+    // Пагинация
+    const limit = parseInt(size, 10)
+    const offset = (parseInt(page, 10) - 1) * limit
+
+    const meetups = await Meetup.findAll({
+      where: filter,
+      order,
+      limit,
+      offset,
+    })
+
+    const totalMeetups = await Meetup.count({ where: filter })
+    const totalPages = Math.ceil(totalMeetups / limit)
+
+    res.status(200).json({
+      data: meetups,
+      meta: {
+        totalMeetups,
+        totalPages,
+        currentPage: parseInt(page, 10),
+      },
+    })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
 }
 
 exports.getMeetupById = async (req, res) => {
